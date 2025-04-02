9df9d1ebf566fc2cff396af92cebc62164ba6d14
PLUS\Angela.Delapena <angela.delapena@tbu.net>
2025-04-02 17:06:26 +0800

    setup shopping list state

---
+++ lib/shopping_list/bloc/shopping_list_state.dart
@@ -0,0 +1,7 @@
+import 'package:shopping_list/shopping_list/models/item.dart';
+
+class ShoppingListState {
+  final List<Item> shoppingList;
+
+  ShoppingListState({required this.shoppingList});
+}
